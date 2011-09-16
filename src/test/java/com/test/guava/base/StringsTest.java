db7d5e27868e39fe12337c2c816a7edab11a65e7
Wei Li <weili.mail@gmail.com>
2011-09-15 20:04:30 -0400

    Let the test start.

--- src/test/java/com/test/guava/base/StringsTest.java
+++ src/test/java/com/test/guava/base/StringsTest.java
@@ -1,7 +1,8 @@
 package com.test.guava.base;
 
-import junit.framework.Assert;
 import org.junit.Test;
+import static junit.framework.Assert.*;
+
 import  static com.google.common.base.Strings.*;
 
 public class StringsTest {
@@ -9,7 +10,30 @@ public class StringsTest {
 	@Test
 	public void testIsNullOrEmpty() {
 		String temp = "";
-		Assert.assertTrue(isNullOrEmpty(temp));
+		assertTrue(isNullOrEmpty(temp));
+		
+		temp = null;
+		assertTrue(isNullOrEmpty(temp));
+		
+		temp = " ";
+		assertFalse(isNullOrEmpty(temp));
+	}
+	
+	@Test
+	public void testEmptyToNull() {
+		String temp = "";
+		assertNull(emptyToNull(temp));
+		
+		temp = "  ";
+		assertNotNull(emptyToNull(temp));
+	}
+	
+	@Test 
+	public void testNullToEmpty() {
+		String temp = null;
+		assertEquals("", nullToEmpty(temp));
+		assertSame("", nullToEmpty(temp));
+		assertNotSame(" ", nullToEmpty(temp)); 
 	}
 
 }
